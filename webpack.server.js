d63e196e7f7fbee2781969cf09fbfd8f296b7955
thinktwice13 <marioo.krajacic@gmail.com>
2017-10-15 17:02:58 +0200

    Config server webpack

--- webpack.server.js
+++ webpack.server.js
@@ -14,7 +14,7 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          preset: [
+          presets: [
             "react",
             "stage-0",
             ["env", { targets: { browsers: ["last 2 versions"] } }]
