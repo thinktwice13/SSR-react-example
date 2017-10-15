b2ba98ae6d9207b4506da51f37b4ad83a8e6a50a
thinktwice13 <marioo.krajacic@gmail.com>
2017-10-15 21:12:46 +0200

    Fetch users list

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const apiUrl = "https://react-ssr-api-herokuapp.com"
+const apiUrl = "https://react-ssr-api.herokuapp.com"
 
 export const FETCH_USERS = "fetch_users"
 export const fetchUsers = () => async dispatch => {
---
+++ src/client/components/UsersList.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { fetchUsers } from "../actions"
+
+class UsersList extends Component {
+  componentDidMount() {
+    this.props.fetchUsers()
+  }
+
+  renderUsers() {
+    return this.props.users.map(user => <li key={user.id}>{user.name}</li>)
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ users }) => ({ users })
+
+export default connect(mapStateToProps, { fetchUsers })(UsersList)
