7421936552cfdf7ebf0ddc57d441e1cbb519ae54
thinktwice13 <marioo.krajacic@gmail.com>
2017-10-15 16:51:24 +0200

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const React = require("react")
+const renderToString = require("react-dom/server").renderToString
+const Home = require("./client/components/Home").default
+
+const app = express()
+
+app.get("/", (req, res) => {
+  const content = renderToString(<Home />)
+  res.send(content)
+})
+
+app.listen(3003, () => console.log("Server started."))
+ 
