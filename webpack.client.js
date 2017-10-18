ac0c251645989fdedb6098600e6e8f7ae6a9fd66
thinktwice13 <marioo.krajacic@gmail.com>
2017-10-18 11:27:03 +0200

    Adjust for heroku deployment

--- webpack.client.js
+++ webpack.client.js
@@ -1,13 +1,30 @@
+const webpack = require("webpack")
+const merge = require("webpack-merge")
 const path = require("path")
 const baseConfig = require("./webpack.base.js")
-const merge = require("webpack-merge")
-
 const config = {
-  entry: "./src/client/index.js",
+  entry: "./src/client/App.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
   }
 }
-
-module.exports = merge(baseConfig, config)
+let mergedConfig = merge(baseConfig, config)
+if (process.env.NODE_ENV === "production") {
+  const prod = {
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify("production") // derp :)
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  }
+  mergedConfig = merge(mergedConfig, prod)
+}
+module.exports = mergedConfig
